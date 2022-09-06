977ffb236b7d96cec886bd004e23fac007fd8a0d
dev-shuvodip <88487718+dev-shuvodip@users.noreply.github.com>
2022-09-06 13:52:34 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,8 @@
-import {
-  AfterViewInit,
-  Component,
-  ViewChild
-} from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { PageEvent } from '@angular/material/paginator';
 import { Observable } from 'rxjs';
-import {
-  map,
-  startWith,
-  switchMap
-} from 'rxjs/operators';
+import { map, startWith, switchMap } from 'rxjs/operators';
 
 import { ApiPaginationService } from './api-pagination.service';
 import { User, UserData } from './user.model';
@@ -18,9 +10,8 @@ import { User, UserData } from './user.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent implements AfterViewInit {
   errorMessage!: string;
   userData = new Observable<UserData>();
@@ -33,31 +24,34 @@ export class AppComponent implements AfterViewInit {
   pageEvent!: PageEvent;
   isLoadingResults = true;
   displayedColumns: string[] = ['id', 'title', 'firstName', 'lastName'];
-  dataSource: User[] = []
+  dataSource: User[] = [];
 
   @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
 
-  constructor(private apiPaginationService: ApiPaginationService) { }
+  constructor(private apiPaginationService: ApiPaginationService) {}
 
   ngAfterViewInit(): void {
-    this.paginator.page.pipe(
-      startWith({}),
-      switchMap(() => {
-        this.isLoadingResults = true;
-        return this.apiPaginationService.getUserData(
-          this.paginator.pageIndex, this.paginator.pageSize
-        );
-      }),
-      map(data => {
-        this.isLoadingResults = false;
-        if (data === null) {
-          return [];
-        }
+    this.paginator.page
+      .pipe(
+        startWith({}),
+        switchMap(() => {
+          this.isLoadingResults = true;
+          return this.apiPaginationService.getUserData(
+            this.paginator.pageIndex,
+            this.paginator.pageSize
+          );
+        }),
+        map((data) => {
+          this.isLoadingResults = false;
+          if (data === null) {
+            return [];
+          }
 
-        this.total = data.total;
-        this.limit = data.limit
-        return data.data;
-      })
-    ).subscribe((data: User[]) => (this.dataSource = data));
+          this.total = data.total;
+          this.limit = data.limit;
+          return data.data;
+        })
+      )
+      .subscribe((data: User[]) => (this.dataSource = data));
   }
 }
